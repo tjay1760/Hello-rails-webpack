7ca8f5828f45fcd441dffae0152131dd555719ae
tjay1760 <mcjthiongo@gmail.com>
2023-10-25 21:00:55 +0300

    Add store and display greeting

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'; // Adjust the path if necessary
+import App from './components/App'; 
+import { Provider } from 'react-redux';
+import store from './store.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <Provider store={store}>
+        <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
